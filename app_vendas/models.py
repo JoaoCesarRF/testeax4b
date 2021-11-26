821e7108fab76790dd5cca4ae9a4b4f5a7a5a494
João Cesar <joaocesar.rizzatto85@gmail.com>
2021-11-26 16:04:31 -0300

    primary key auto

--- app_vendas/models.py
+++ app_vendas/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 
 class Venda(models.Model):
-    cod_venda = models.CharField(max_length=5, auto_created=True, primary_key=True, blank=True)
+    cod_venda = models.IntegerField(primary_key=True, blank=True)
     cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
     produtos = models.ForeignKey('Produto', on_delete=models.CASCADE)
 
 
 class Cliente(models.Model):
-    cod_cliente = models.CharField(max_length=5, auto_created=True, primary_key=True, blank=True)
+    cod_cliente = models.IntegerField(primary_key=True, blank=True)
     nome = models.CharField(max_length=100)
     endereco = models.CharField(max_length=150)
     email = models.EmailField
     telefone = models.CharField(max_length=15)
-    whatss = models.BooleanField
+    whats = models.BooleanField
     data_cadastro = models.DateTimeField(auto_now_add=True)
 
 
 class Produto(models.Model):
-    cod_produto = models.CharField(max_length=5, auto_created=True, primary_key=True, blank=True)
+    cod_produto = models.IntegerField(primary_key=True, blank=True)
     descricao_produto = models.CharField(max_length=100)
     preco = models.IntegerField
