596cd888a6a1fcff9a9555dc6fbb1c1a994dbe5e
João Cesar <joaocesar.rizzatto85@gmail.com>
2021-11-26 16:58:26 -0300

    detalhes modelagem banco

--- app_vendas/models.py
+++ app_vendas/models.py
@@ -5,25 +5,30 @@ class Venda(models.Model):
     cod_venda = models.IntegerField(primary_key=True, blank=True)
     cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
     produtos = models.ForeignKey('Produto', on_delete=models.CASCADE)
-    observacao = models.TextField()
+    observacao = models.TextField
+
     def __str__(self):
-        return self.cod_venda
+        return self.cod_venda.__str__()
+
 
 class Cliente(models.Model):
     cod_cliente = models.IntegerField(primary_key=True, blank=True)
     nome = models.CharField(max_length=100)
     endereco = models.CharField(max_length=150)
-    email = models.EmailField()
+    email = models.EmailField
     telefone = models.CharField(max_length=15)
-    whats = models.BooleanField()
+    whats = models.BooleanField
     data_cadastro = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.nome
 
+
 class Produto(models.Model):
     cod_produto = models.IntegerField(primary_key=True, blank=True)
-    titulo = models.CharField(max_length=15)
-    descricao_produto = models.TextField()
-    preco = models.DecimalField(max_digits=7,decimal_places=2)
+    titulo = models.CharField(max_length=20, default='Titulo')
+    descricao_produto = models.TextField(default='Descreva o Produto')
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+
     def __str__(self):
         return self.titulo
