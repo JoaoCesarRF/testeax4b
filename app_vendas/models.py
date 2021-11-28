dfd29c615d269ef08f2c34446c7049e988f1ace1
João Cesar <joaocesar.rizzatto85@gmail.com>
2021-11-27 21:11:18 -0300

    acertado detalhes modelagem banco

--- app_vendas/models.py
+++ app_vendas/models.py
@@ -5,7 +5,15 @@ class Venda(models.Model):
     cod_venda = models.IntegerField(primary_key=True, blank=True)
     cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
     produtos = models.ForeignKey('Produto', on_delete=models.CASCADE)
-    observacao = models.TextField
+    observacao = models.TextField(default=None, blank=True)
+
+    class TipoPagamento(models.TextChoices):
+        VISA = 'Visa', ('Visa')
+        BOLETO = 'Boleto', ('Boleto')
+        MASTER = 'Mastercard', ('Mastercard')
+
+    tipo_pagamento = models.CharField(max_length=10, choices=TipoPagamento.choices, default='Visa',
+    verbose_name='Meio de Pagamento')
 
     def __str__(self):
         return self.cod_venda.__str__()
@@ -15,9 +23,10 @@ class Cliente(models.Model):
     cod_cliente = models.IntegerField(primary_key=True, blank=True)
     nome = models.CharField(max_length=100)
     endereco = models.CharField(max_length=150)
-    email = models.EmailField
+    email = models.EmailField(default=None)
+    whats = models.BooleanField(default=None)
     telefone = models.CharField(max_length=15)
-    whats = models.BooleanField
+
     data_cadastro = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
